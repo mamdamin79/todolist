7b2ac2842fc06a2a7354224a718a2181b82bd929
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-08-15 07:57:32 -0700

    pushing commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getLocalStorageData = () =>{
+  if (localStorage.getItem("todos")) {
+    return JSON.parse(localStorage.getItem("todos"))
+  }
+  else{
+    return [];
+  }
 }
 
-export default App;
+export default function App() {
+  const [todos, updateTodos] = React.useState(getLocalStorageData());
+  const [count, setCount] = React.useState(todos.length);
+  const [value, setValue] = React.useState("")
+  
+
+  React.useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos])
+
+  /** Problem 1: why page gets refreshed? */
+  /** Problem 2: why count gets wrong updates? */
+  function addTodo() {
+    updateTodos([...todos,{title : value , id: Math.random() * 100}]);
+    setCount((prev)=> prev + 1);
+    setValue("")
+  }
+
+  /** Problem 3: why todos don't get deleted? */
+  const removeTodo = (id) => {
+    const newTodos = todos.filter(todo=> todo.id !== id);
+    updateTodos(newTodos);
+    setCount((prev)=> prev - 1);
+
+  }
+
+  /** Bonus Problem: Items clear on page reload. */
+
+  return (
+    <main>
+      <form  onSubmit={(e)=>e.preventDefault()}>
+        <input value={value} onChange={(e)=>setValue(e.target.value)} type="text" name="todo" />
+        <button type="submit" onClick={addTodo}>
+          add
+        </button>
+        <span style={{ marginInline: '1em' }}>({count})</span>
+      </form>
+
+      <section>
+        <ul>
+          {todos.map(todo => (
+            <li
+              key={todo.id}
+              style={{ cursor: 'pointer' }}
+              onClick={() => removeTodo(todo.id)}
+            >
+              {todo.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
